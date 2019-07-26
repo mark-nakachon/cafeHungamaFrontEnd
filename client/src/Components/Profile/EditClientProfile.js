ad4274c4164a763d7e3f6b16c15a68dcf6fb95f6
Rishi Singhal <rishisinghal10@gmail.com>
2019-07-26 15:21:43 +0530

    Transaction Details Addtion features given to admin and profile validations improved

--- client/src/Components/Profile/EditClientProfile.js
+++ client/src/Components/Profile/EditClientProfile.js
@@ -1,30 +1,30 @@
 import React from 'react'
-import { Form,  Input, Button } from 'antd';
+import { Form, Input, Button } from 'antd';
 import FormItem from 'antd/lib/form/FormItem';
 import API from '../../api/API';
 
 class EditClientProfileForm extends React.Component {
 
-   state={
-      data:this.props.data        // data variable contains jason before and after Edit
-   }
-  onEdit=async value=>{                    // This function  will Call the API (Post request)
-    let profile={
-      firstName:value.firstName,
-      lastName:value.lastName,
-      contact:value.contact,
-      alternateContact:value.alternateContact,
-      line1Add:value.line1Add,
-      line2Add:value.line2Add,
-      email:value.email,
-      state:value.state,
-      city:value.city
-      }
+  state = {
+    data: this.props.data        // data variable contains jason before and after Edit
+  }
+  onEdit = async value => {                    // This function  will Call the API (Post request)
+    let profile = {
+      firstName: value.firstName,
+      lastName: value.lastName,
+      contact: value.contact,
+      alternateContact: value.alternateContact,
+      line1Add: value.line1Add,
+      line2Add: value.line2Add,
+      email: value.email,
+      state: value.state,
+      city: value.city
+    }
     // eslint-disable-next-line
-    const response=await API.put('/client/5d368a7f4a915e2c58f34952/profile/',profile)
-    .catch(function (error) {
-      alert("Update Failed")
-    });
+    const response = await API.put('/client/5d368a7f4a915e2c58f34952/profile/', profile)
+      .catch(function (error) {
+        alert("Update Failed")
+      });
     alert("Submitted");
   }
 
@@ -32,83 +32,93 @@ class EditClientProfileForm extends React.Component {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        this.setState({data:values},()=>this.onEdit(this.state.data))
+        this.setState({ data: values }, () => this.onEdit(this.state.data))
       }
     });
-   
+
   };
-  
-  render(){
+
+  render() {
     const { getFieldDecorator } = this.props.form;
     return (
       <div>
-         <Form layout="horizontal" onSubmit={this.log} >
-            <FormItem label="First Name" >
-              {getFieldDecorator("firstName", {
-                rules: [
-                  {
-                    required: true,
-                    message: 'First Name is Required',
-                  }
-                ]
-              })(<Input />)}
-            </FormItem>
-            <FormItem label="Last Name">
-              {getFieldDecorator("lastName")(<Input />)}
-            </FormItem>
-            <FormItem label="Phone Number">
-              {getFieldDecorator("contact", {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Phone Number is Required',
-                  }
-                ]
-              })(<Input />)}
-            </FormItem>
-            <FormItem label="Alternate Phone Number">
-              {getFieldDecorator("alternateContact")(<Input />)}
-            </FormItem>
-            <FormItem label="Address">
-              {getFieldDecorator('line1Add', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Address is Required',
-                  }
-                ]
-              })(<Input placeholder="Line"/>)}
-              {getFieldDecorator('line2Add')(<Input Input placeholder="Line 2"/>)}
-              {getFieldDecorator('city')(<Input placeholder="City"/>)}
-              {getFieldDecorator('state', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'State is Required',
-                  }
-                ]
-              })(<Input Input placeholder="State"/>)}
-            </FormItem>
-            <FormItem label="Email">
-              {getFieldDecorator('email', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Address is Required',
-                  }
-                ]
-              })(<Input />)}
-            </FormItem>
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Edit
+        <Form layout="horizontal" onSubmit={this.log} >
+          <FormItem label="First Name" >
+            {getFieldDecorator("firstName", {
+              rules: [
+                {
+                  required: true,
+                  message: 'First Name is Required',
+                }
+              ]
+            })(<Input />)}
+          </FormItem>
+          <FormItem label="Last Name">
+            {getFieldDecorator("lastName")(<Input />)}
+          </FormItem>
+          <FormItem label="Phone Number">
+            {getFieldDecorator("contact", {
+              rules: [
+                {
+                  required: true,
+                  message: 'Phone Number is Required',
+                  pattern: "[1-9][0-9]{9}"
+                }
+              ]
+            })(<Input />)}
+          </FormItem>
+          <FormItem label="Alternate Phone Number">
+            {getFieldDecorator("alternateContact", {
+              rules: [
+                {
+                  required: false,
+                  message: 'Invalid',
+                  pattern: "[1-9][0-9]{9}"
+                }
+              ]
+            })(<Input />)}
+          </FormItem>
+          <FormItem label="Address">
+            {getFieldDecorator('line1Add', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Address is Required',
+                }
+              ]
+            })(<Input placeholder="Line" />)}
+            {getFieldDecorator('line2Add')(<Input Input placeholder="Line 2" />)}
+            {getFieldDecorator('city')(<Input placeholder="City" />)}
+            {getFieldDecorator('state', {
+              rules: [
+                {
+                  required: true,
+                  message: 'State is Required',
+                }
+              ]
+            })(<Input Input placeholder="State" />)}
+          </FormItem>
+          <FormItem label="Email">
+            {getFieldDecorator('email', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Enter Valid Email',
+                  type: "email"
+                }
+              ]
+            })(<Input />)}
+          </FormItem>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Edit
               </Button>
-            </Form.Item>
-         </Form>
+          </Form.Item>
+        </Form>
       </div>
     )
   }
 }
-const EditClientProfile=Form.create()(EditClientProfileForm)
+const EditClientProfile = Form.create()(EditClientProfileForm)
 
 export default EditClientProfile
