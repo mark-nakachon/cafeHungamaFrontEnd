86a15f4862b60da638fc8160494bbb825900e5d5
unknown <mansigera122@gmail.com>
2019-07-26 16:33:51 +0530

    dashboard

--- client/src/Components/Dashboard/Eventsdone.js
+++ client/src/Components/Dashboard/Eventsdone.js
@@ -1,12 +1,13 @@
 import { Table } from "antd";
 import React from "react";
+import './dashboard.css'
 import axios from "axios";
 
 //make a state for called response for accept and decline and pass that as parameter in axios.post
 class Payment extends React.Component {
     componentDidMount() {
         axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
-            this.setState({ data: res.data });
+            this.setState({ data: res.data, loading: false });
 
             console.log(this.state.data.req);
         });
@@ -14,6 +15,7 @@ class Payment extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            loading: true,
             notifications: [],
             columns: [
                 {
@@ -34,7 +36,7 @@ class Payment extends React.Component {
         const { data } = this.state;
         return (
             <div style={{ marginLeft: '20px', width: '40%' }}>
-                <Table columns={this.state.columns} dataSource={data} bordered size="small" />
+                <Table columns={this.state.columns} dataSource={data} pagination={false} bordered size="large"  scroll={{y:160}} loading={this.state.loading}/>
             </div>
         );
     }
