50030659489142a98b3f1a67ea3718525af0cd19
unknown <mansigera122@gmail.com>
2019-07-31 22:37:05 +0530

    add venue apis added

--- client/src/Components/Venue/Venue.js
+++ client/src/Components/Venue/Venue.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
-import { Card, Icon, Row, Col, Modal } from "antd";
+import { Card, Icon, Row, Col, Modal, Button, Tag } from "antd";
 import AddVenue from "./AddVenue";
 import "./venue.css";
-import Axios from "axios";
+import API from "../../api/API";
 class Venue extends Component {
   state = {
     visible: false,
-    data : []
+    data: []
   };
 
   componentDidMount() {
-    Axios.get(
-      `https://cafehungama.herokuapp.com/client/5d09067224036b46e40f8d30/venues`
-    ).then(res => {
+    API.get(`/client/5d09067224036b46e40f8d30/venues`).then(res => {
       this.setState({ data: res.data });
     });
   }
@@ -35,88 +33,87 @@ class Venue extends Component {
       visible: false
     });
   };
-
   renderData = venues => {
-    if(venues.length>0)
-    return venues.map(venue => {
-      return (
-        <div style={{ alignContent: "center" }}>
-          <Card.Grid
-            style={{
-              width: "100%",
-              display: "flex",
-              flexWrap: "wrap",
-              justifyContent: "space-around",
-              flexDirection: "column",
-              backgroundColor: "#dbd7d7",
-              border: "1px solid black",
-              margin: "5px"
-            }}
-          > {/* <div className="vimage">
-            <img src={venue.image} />
-          </div> */}
-            <div className="vdetail">
-              <h4>Name:{venue.venueName}</h4>
-              <h4>Address:{venue.line1},{venue.city},{venue.state}</h4>
-              <h4>Ratings:</h4>
-              <h4>Screens:{venue.noOfScreens}</h4>
-              <h4>Amenties:{venue.amenties}</h4>
-              <h4>Contact:{venue.venueContact}</h4>
-              <h4>Capacity:{venue.maxcapacity}</h4>
-            </div>
-          </Card.Grid>
-        </div>
-      );
-    });
-    else
-    return <div>No Venues Registered</div>
+    if (venues.length > 0)
+      return venues.map(venue => {
+        return (
+          <div>
+            <Card.Grid className="venueslist">
+              {" "}
+              <div className="vimage">{venue.image}</div>
+              <div className="vdetail">
+                <h4>Name:{venue.venueName}</h4>
+                <h4>
+                  Address:{venue.line1},{venue.city},{venue.state}
+                </h4>
+                <h4>Ratings:</h4>
+                <h4>Screens:{venue.noOfScreens}</h4>
+                <h4>Amenties:{venue.amenties}</h4>
+                <h4>Contact:{venue.venueContact}</h4>
+                <h4>Capacity:{venue.maxCapacity}</h4>
+              </div>
+            </Card.Grid>
+          </div>
+        );
+      });
+    else return <div>No Venues Registered</div>;
   };
-  onSubmit=(props)=>{
+  onSubmit = props => {
     console.log(props);
-    Axios.post(
-      `https://cafehungama.herokuapp.com/client/5d09067224036b46e40f8d30/venues`,props[0]
-    ).then(function (response) {
+    API.post(`/client/5d09067224036b46e40f8d30/venues`, props[0]).then(function (
+      response
+    ) {
       console.log(response);
-      console.log( response.data);
-    })
+      console.log(response.data);
+    });
     this.setState({
       visible: false
     });
-    
+
   }
 
   render() {
     return (
       <div>
         <Row>
-          <Col span={12}>
-            <Card
-              title="Manage Venue"
-              headStyle={{
-                textAlign: "center",
-                fontSize: "20px",
-                color: "white",
-                backgroundColor: "#b51a12"
-              }}
-            >
-              {this.renderData(this.state.data)}
-            </Card>
-          </Col>
-          <Col span={12}>
-            <button class="button" onClick={this.showModal}>
-              <Icon type="home" />
-              Add new Venue
-            </button>
-
+          <Row>
+            <Tag color="geekblue" className="listofvenues">
+              List of Venues
+            </Tag>
+            <Row />
+            <Col>
+              <Card>{this.renderData(this.state.data)}</Card>
+            </Col>
+            <br />
+            <Row>
+              <Button
+                type="primary"
+                onClick={this.showModal}
+                style={{ margin: "10px" }}
+              >
+                <Icon type="environment" />
+                Add new Venue
+              </Button>
+            </Row>
             <Modal
               title="Add Venue"
               visible={this.state.visible}
               onOk={this.handleOk}
               onCancel={this.handleCancel}
             >
-              <AddVenue onSubmit={this.onSubmit}/>
+              <AddVenue onSubmit={this.onSubmit} />
             </Modal>
-          </Col>
+
+            <Col span={12} />
+          </Row>
+          <div>
+            <Modal
+              title="Venue Detail"
+              visible={this.state.visibleDetails}
+              onOk={this.handleOkDetails}
+              onCancel={this.handleCancelDetails}
+            />
+          </div>
         </Row>
       </div>
     );
