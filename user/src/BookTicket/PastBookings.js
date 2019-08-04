44a8b1c21c0d834e9232fe20d66064d8af833398
mark-nakachon <harishchennupati54@gmail.com>
2019-08-04 10:34:15 +0530

    Added Past Bookings and My orders

--- user/src/BookTicket/PastBookings.js
+++ user/src/BookTicket/PastBookings.js
@@ -1,9 +1,9 @@
-import { Skeleton, Switch, Card, Icon, Avatar,Row,Col } from 'antd';
+import { Skeleton, Switch, Card, Icon, Avatar,Row,Col,Select } from 'antd';
 import Loader from 'react-loader-spinner';
 import React from 'react';
 import {withContext} from '../Context';
 const { Meta } = Card;
-
+const {Option} = Select;
 
 class PastBookings extends React.Component {
   state = {
@@ -14,6 +14,11 @@ class PastBookings extends React.Component {
   onChange = checked => {
     this.setState({ loading: !checked });
   };
+
+  handleChange = value => {
+    console.log(`selected ${value}`);
+  }
+
   componentDidMount(){
     const bearer = 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjVkNDFiY2Q2MzZhYWU3Mzg0MTZmZGQxMSIsImVtYWlsIjoiaGFyaXNoY2hlbm51cGF0aTJAZ21haWwuY29tIn0sImlhdCI6MTU2NDY2MDE4MX0.bO90AbCLVJY3P9UPX3x8WKYTl4FW3Glt-XTMeieyifg';
     fetch(`http://localhost:5000/user/bookings/5d342f20bac84723fd192b84`,{
@@ -42,7 +47,13 @@ class PastBookings extends React.Component {
             <h1>My Past Bookings</h1>
           </div>
           <Row type="flex" justify="center">
-
+            <Col span={4}>
+                <Select size={'large'} defaultValue="Filter by event types" style={{width:'100%',marginLeft:'30%',marginTop:'10%'}} onChange={this.handleChange}>
+                  <Option value="Comedy">Comedy</Option>
+                  <Option value="Movie">Movie</Option>
+                </Select>
+            </Col>
+            <Col span={20}>
             {
               (data==='')?(
                 <Loader type="ThreeDots" color="#000000" height={200} width={200} />)
@@ -50,11 +61,10 @@ class PastBookings extends React.Component {
                 <React.Fragment>{
                   data.map(details=>{
                     return (
-                    <Col span={18} style={{marginTop:'2%'}}>
                     <Card title="12 July"
-                style={{ width:'100%'}}
+                style={{ width:'80%',margin:'auto',marginTop:'2%'}}
                   >
-                      <Row type="flex" justify="space-around">
+                      <Row type="flex" justify="space-between">
                           <Col span={4}>
                           <h3>{details.eventType}</h3>
                           </Col>
@@ -73,13 +83,13 @@ class PastBookings extends React.Component {
 
                       </Row>
                   </Card>
-                    </Col>);
+                  );
                   })
                 }
                 </React.Fragment>
               )
             }
-
+            </Col>
           </Row>
       </div>
     );
